0fcc4160922dc0747502c93697a47f0fc05d78bb
LaryssaS2 <91742929+LaryssaS2@users.noreply.github.com>
2022-03-25 19:40:10 -0300

    Create dados.php

---
+++ dados.php
@@ -0,0 +1,19 @@
+<?php
+    if (isset($_POST["acao"])){
+        $nome=$_POST["nome"];
+        $telefone=$_POST["telefone"];
+        $email=$_POST["email"];
+        $radio=$_POST["novidades"];
+        $msg=$_POST["mensagem"];
+
+        echo "<p>Olá, ".$nome."</p>";
+        echo "<p>Seu email é: ".$email."</p>";
+        echo "<p>Seu telefone é: ".$telefone."</p>";
+        if ($radio=="sim"){
+            echo "<p>Eu já trabalhei</p>";
+        }elseif ($radio=="nao") {
+            echo "<p>Eu nunca trabalhei</p>";
+        }
+        echo "<p>Sua mensagem é:<br/>".$msg."</p>";
+    }
+?>
